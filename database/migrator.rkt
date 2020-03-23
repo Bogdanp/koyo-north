a552fe709655a15d1a1382a909fe2466f173f27c
Bogdan Popa <bogdan@defn.io>
2020-03-23 11:45:00 +0200

    migrator: display underlying exception

--- database/migrator.rkt
+++ database/migrator.rkt
@@ -32,17 +32,23 @@
 
 (define (migrate! m)
   (match-define (migrator path db) m)
-  (call-with-database-connection db
-    (lambda (conn)
-      (define base (path->migration path))
-      (define adapter
-        (match (dbsystem-name (connection-dbsystem conn))
-          ['sqlite3 (sqlite-adapter conn)]
-          ['postgresql (postgres-adapter conn)]
-          [name (error 'migrate "db system not supported: ~a" name)]))
+  (with-handlers ([exn:fail:adapter:migration?
+                   (lambda (e)
+                     (error 'migrate!
+                            "failed to apply revision ~a\n  cause: ~a"
+                            (exn:fail:adapter:migration-revision e)
+                            (exn-message (exn:fail:adapter-cause e))))])
+    (call-with-database-connection db
+      (lambda (conn)
+        (define base (path->migration path))
+        (define adapter
+          (match (dbsystem-name (connection-dbsystem conn))
+            ['sqlite3 (sqlite-adapter conn)]
+            ['postgresql (postgres-adapter conn)]
+            [name (error 'migrate "db system not supported: ~a" name)]))
 
-      (adapter-init adapter)
-      (define current (adapter-current-revision adapter))
-      (define target (migration-revision (migration-most-recent base)))
-      (for ([migration (in-list (migration-plan base current target))])
-        (adapter-apply! adapter (migration-revision migration) (migration-up migration))))))
+        (adapter-init adapter)
+        (define current (adapter-current-revision adapter))
+        (define target (migration-revision (migration-most-recent base)))
+        (for ([migration (in-list (migration-plan base current target))])
+          (adapter-apply! adapter (migration-revision migration) (migration-up migration)))))))
