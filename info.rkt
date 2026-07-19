ac7581205193f1f06207107beb1580fd42d66dfe
Bogdan Popa <bogdan@defn.io>
2026-07-19 15:57:05 +0300

    build: require north 0.9+ and bump version
    To get the locking changes.

--- info.rkt
+++ info.rkt
@@ -1,10 +1,10 @@
 #lang info
 
 (define license 'BSD-3-Clause)
-(define version "0.6")
+(define version "0.7")
 (define collection "koyo")
 (define deps
   '("base"
     "component-lib"
     "koyo-lib"
-    ["north" #:version "0.6"]))
+    ["north" #:version "0.9"]))
